361789f0c930d2b963857b334243a21d6b47d00a
Landon DeCoito <rdecoito@gmu.edu>
2021-10-20 20:30:01 -0400

    Cont.

--- src/Components/PcManager.svelte
+++ src/Components/PcManager.svelte
@@ -1,4 +1,23 @@
 <script lang="ts">
-	export let pcs: Array<PlayerCharacter> = [];
+	import type { PlayerCharacter } from '../global';
 
+	export let pcs: Array<PlayerCharacter> = [];
 </script>
+
+<div class="npcs-block">
+	{#each npcs as npc, index (npc)}
+	<div class="Card-container">
+		<Card>
+			<div class="button-row">
+				<button on:click={() => handleEraseNpc(index)}>X</button>
+				<button on:click={() => handleResetNpc(index)}>{'<-'}</button>
+			</div>
+			<Npc npc={npc} />
+		</Card>
+	</div>
+	{/each}
+</div>
+
+<style lang="scss">
+	
+</style>
---
+++ src/global.ts
@@ -0,0 +1,106 @@
+/// <reference types="@sveltejs/kit" />
+
+/* ========= ========= ========= */
+/* ========= CONSTANTS ========= */
+/* ========= ========= ========= */
+
+export enum MasteryLevel {
+	LEARNED = 'Learned',
+	PRACTICED = 'Practiced',
+	MASTERED = 'Mastered'
+};
+
+export enum Approach {
+	DM = 'D&M',
+	AA = 'A&A',
+	EO = 'E&O'
+};
+
+export enum NpcImportance {
+	/**
+	 * An NPC who may participate in conversations or fights but is unlikely to reappear in the adventure
+	 */
+	MINOR = 'Minor',
+	/**
+	 * An NPC who may be the focus of a session, but likely not the focus of an entire adventure
+	 */
+	MODERATE = 'Moderate',
+	/**
+	 * An NPC who may be a significant recurring character and may be the focus of an adventure
+	 */
+	MAJOR = 'Major'
+};
+
+export const NpcMaxStats = Object.freeze({
+	[NpcImportance.MINOR]: Object.freeze({
+		fatigue: 3 as Fatigue<NpcImportance.MINOR>,
+		conditions: 1,
+		balance: 1 as Balance<NpcImportance.MINOR>
+	}),
+	[NpcImportance.MODERATE]: Object.freeze({
+		fatigue: 5 as Fatigue<NpcImportance.MODERATE>,
+		conditions: 3,
+		balance: 2 as Balance<NpcImportance.MODERATE>
+	}),
+	[NpcImportance.MAJOR]: Object.freeze({
+		fatigue: 10 as Fatigue<NpcImportance.MAJOR>,
+		conditions: 5,
+		balance: 3 as Balance<NpcImportance.MAJOR>
+	})
+});
+
+/* ========= ===== ========= */
+/* ========= TYPES ========= */
+/* ========= ===== ========= */
+
+export type Fatigue<T extends NpcImportance> = T extends NpcImportance.MINOR ? 0 | 1
+	: T extends NpcImportance.MODERATE ? 0 | 1 | 2 | 3 | 4 | 5
+	: 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
+
+export type Condition = [string, boolean];
+export type Conditions = Array<Condition>;
+
+export type Balance<T extends NpcImportance> = T extends NpcImportance.MINOR ? 0 | 1
+	: T extends NpcImportance.MODERATE ? 0 | 1 | 2
+	: 0 | 1 | 2 | 3;
+
+export interface NonPlayerCharacter<T extends NpcImportance = NpcImportance> {
+	/** The name of the NPC */
+	name: string;
+	/** The importance level of the NPC */
+	importance: T;
+	/** The principle the NPC  tries to embody */
+	principle: string;
+	/** The drive the NPC works toward. Must be in the format 'To <something>' */
+	drive: `To ${string}`;
+	/** A mapping between the names of possible conditions and whether the condition is marked */
+	conditions: Conditions;
+	/** The amount of fatigue the NPC has marked */
+	fatigue: Fatigue<T>;
+	/** The position of the NPCs balance in the direction of their principle */
+	balance: Balance<T>;
+}
+
+export interface FightingTechnique {
+	name: string;
+	approach: Approach;
+	mastery: MasteryLevel;
+}
+
+export interface PlayerStats {
+	creativity: number;
+	focus: number;
+	harmony: number;
+	passion: number;
+}
+
+export interface PlayerCharacter {
+	playbook: string;
+	trainings: Array<string>;
+	fightingStyle: string;
+	stats: PlayerStats;
+	conditions: Array<Condition>;
+	moves: Array<string>;
+	techniques: Array<FightingTechnique>;
+	growth: number;
+}
