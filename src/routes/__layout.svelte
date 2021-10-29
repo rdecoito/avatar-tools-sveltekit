d4029620a540f668efcd3c447040e5a5593efeec
Landon DeCoito <rdecoito@gmu.edu>
2021-10-28 22:14:25 -0400

    added favicon and logo in navbar

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -8,8 +8,12 @@
 <ThemeWrapper mode="dark" {themes}>
 	<CharacterStoreProvider>
 		<div class="navbar">
-			<a href="/about" class="circle-button">
+			<a href="/about" class="circle-icon button">
 				<i class="fas fa-question" />
+			</a>
+			<a href="/" class="rounded button">
+				<img src="/favicon-32x32.png" alt="Check Your Balance" />
+				&nbsp;Check Your Balance
 			</a>
 			<div style="display: flex; flex-flow: row nowrap;">
 				<DataImportExportButtons />
@@ -30,17 +34,41 @@
 		padding: var(--theme-space-separation);
 	}
 
-	a.circle-button {
+	a.rounded {
+		display: flex;
+		flex-flow: row nowrap;
+		align-items: center;
+
+		border-radius: 0.75rem;
+		padding: 0.25rem 1rem;
+	}
+
+	a:link,
+	a:visited,
+	a:hover,
+	a:active {
+		text-decoration: none;
+	}
+
+	a.circle-icon {
 		display: flex;
 		flex-flow: column;
 		justify-content: center;
 		align-items: center;
 
-		color: var(--theme-colors-text);
-		background-color: var(--theme-colors-background-accent);
 		border-radius: 0.75rem;
 		height: 1.5rem;
 		width: 1.5rem;
 		font-size: x-small;
+	}
+
+	a.button {
+		color: var(--theme-colors-text);
+		background-color: var(--theme-colors-background-accent);
+	}
+
+	img {
+		width: 1rem;
+		height: 1rem;
 	}
 </style>
